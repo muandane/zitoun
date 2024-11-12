a7a2ee4ffbb3385f73071fed065799bdd6f153a5
Zine Moualhi 🇵🇸 <zmoualhi@outlook.com>
2024-11-12 19:58:18 +0100

    feat: added deletion route

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ export const config = {
 	ZITADEL_JWKS_ENDPOINT: process.env.ZITADEL_JWKS_URI,
 	ZITADEL_PAT: process.env.ZITADEL_PAT,
 	ORIGIN: process.env.CORS_ALLOWED_ORIGIN,
-	DEBUG: process.env.DEBUG,
+	DEBUG: process.env.DEBUG || false,
 	API_PREFIX: process.env.API_PREFIX || "/v1",
 	PORT: Number(process.env.PORT) || 3000,
 };
